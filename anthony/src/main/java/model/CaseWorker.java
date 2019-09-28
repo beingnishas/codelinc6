7cd5b0cd66a74d76d3d1c5be2a381d441d0e1fbe
Kaleb Dean <kad9603@gmail.com>
2019-09-28 18:37:01 -0400

    models created

---
+++ anthony/src/main/java/model/CaseWorker.java
@@ -0,0 +1,50 @@
+package model;
+
+import java.util.ArrayList;
+ 
+public class CaseWorker 
+{
+	String name;
+    String organization;
+
+    ArrayList<Veteran> veteranList = new ArrayList<Veteran>();
+
+    
+    // setter annotation here...
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+
+    public void setOrganization(String organization)
+    {
+        this.organization = organization;
+    }
+
+    public void setVeteranList(ArrayList<Veteran> veteranList)
+    {
+        this.veteranList = veteranList;
+    }
+
+
+    // getter annotation here...
+    public String getName()
+    {
+        return this.name;
+    }
+
+    public String getOrganization()
+    {
+        return this.organization;
+    }
+
+    public ArrayList<Veteran> getVeteranList()
+    {
+        return this.veteranList;
+    }
+
+    public static void main(String args[])
+    {
+        System.out.println("Hello world!");
+    }
+}
