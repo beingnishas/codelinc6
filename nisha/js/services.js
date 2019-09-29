b1f71784bc4af5a5b5d658dcb5c18801db6a80ba
Malcolm R. Jones <maljonesdev@gmail.com>
2019-09-28 23:13:04 -0400

    First attempt at API call

--- nisha/js/services.js
+++ nisha/js/services.js
@@ -1,1 +1,16 @@
-console.log("Hello World")
+const HTTP = new XMLHttpRequest();
+const URL = "https://postman-echo.com/get";
+
+var searchButton = document.getElementById("search-btn");
+
+searchButton.onclick = function() {
+    
+    HTTP.open("GET", URL);
+    HTTP.send();
+    
+    HTTP.onreadystatechange = function() {
+        if(this.readyState == 4 && this.status == 200) {
+            console.log(HTTP.responseText);
+        }
+    }
+}
