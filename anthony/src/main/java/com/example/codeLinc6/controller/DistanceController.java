1a144d575939d1d6cf0316dd3e954b92e3ba2efa
anthonyzayas <anthonyczayas@gmail.com>
2019-09-29 01:49:46 -0400

    Create new service to calculate distance of zip codes

---
+++ anthony/src/main/java/com/example/codeLinc6/controller/DistanceController.java
@@ -0,0 +1,37 @@
+package com.example.codeLinc6.controller;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@Component
+@RestController
+@RequestMapping("/distance")
+public class DistanceController {
+
+	
+	
+	@GetMapping("/test")
+	@ResponseBody
+	public String getDistance(@RequestParam("zip_code_1") String zipCodeOne, @RequestParam("zip_code_2") String zipCodeTwo){
+		
+		String url = "https://www.zipcodeapi.com/rest/Qr9TaO93PSJ7t4WV1l0phHREmKD8GhpD1iVQ0lJGYyHt2Yg0HxPOOcZb9cPEM30t/distance.json/" 
+		+ zipCodeOne + "/" + zipCodeTwo + "mile";
+		RestTemplate restTemplate = new RestTemplate();
+		String result = restTemplate.getForObject(url, String.class);
+		return result;
+	}
+	
+	static public class test {
+		String name = "HI MY NAME IS ANTHONY";
+		
+		public String getName() {
+			return this.name;
+		}
+	}
+}
+
