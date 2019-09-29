c816a7358eaaa808fa2231abe0cda1c3305e5f68
anthonyzayas <anthonyczayas@gmail.com>
2019-09-28 20:55:02 -0400

    Added extra test example

--- anthony/src/main/java/com/example/codeLinc6/CodeLinc6Application.java
+++ anthony/src/main/java/com/example/codeLinc6/CodeLinc6Application.java
@@ -4,56 +4,56 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
-import com.example.codeLinc6.model.*;
-import java.util.ArrayList;
+//import com.example.codeLinc6.model.*;
+//import java.util.ArrayList;
 @SpringBootApplication
-@ComponentScan("com.example.CodeLinc6")
+@ComponentScan("com.example.codeLinc6.")
 public class CodeLinc6Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CodeLinc6Application.class, args);
 		
-		com.example.codeLinc6.model.Veteran myVeteran = new com.example.codeLinc6.model.Veteran();
-		
-		myVeteran.setName("James Barksdale Sr.");
-		myVeteran.setBirthdate("06/27/2001");
-		myVeteran.setZip(28277);
-		myVeteran.setSSN("*****3456");
-		myVeteran.setBranch("Marine Corps");
-		
-		myVeteran.setServiceDates("9/28/2019", "9/30/2019");
-		
-		ArrayList<String> myConflicts = new ArrayList<String>();
-		myConflicts.add("Iraq");
-		myConflicts.add("Afghanistan");
-		myVeteran.setConflicts(myConflicts);
-		
-		ArrayList<String> myDisabilities = new ArrayList<String>();
-		myDisabilities.add("PTSD");
-		myVeteran.setDisabilities(myDisabilities);
-		
-		ArrayList<String> myNeeds = new ArrayList<String>();
-		myNeeds.add("Food");
-		myNeeds.add("Clothing");
-		myNeeds.add("Housing");
-		myVeteran.setNeeds(myNeeds);
-		
-		ArrayList<String> myHistory = new ArrayList<String>();
-		myHistory.add("null");
-		myVeteran.setHistory(myHistory);
-		
-		
-		System.out.println("myVeteran.name: " + myVeteran.getName());
-		
-		CaseWorker myCaseWorker = new CaseWorker();
-		
-		myCaseWorker.setName("Sicko Mo Bamba");
-		myCaseWorker.setOrganization("Vet Foundation");
-		ArrayList<Veteran> myVeteranList = new ArrayList<Veteran>();
-		myVeteranList.add(myVeteran);
-		myCaseWorker.setVeteranList(myVeteranList);
-		
-		System.out.println("myCaseWorker.organization: " + myCaseWorker.getOrganization());
+//		com.example.codeLinc6.model.Veteran myVeteran = new com.example.codeLinc6.model.Veteran();
+//		
+//		myVeteran.setName("James Barksdale Sr.");
+//		myVeteran.setBirthdate("06/27/2001");
+//		myVeteran.setZip(28277);
+//		myVeteran.setSSN("*****3456");
+//		myVeteran.setBranch("Marine Corps");
+//		
+//		myVeteran.setServiceDates("9/28/2019", "9/30/2019");
+//		
+//		ArrayList<String> myConflicts = new ArrayList<String>();
+//		myConflicts.add("Iraq");
+//		myConflicts.add("Afghanistan");
+//		myVeteran.setConflicts(myConflicts);
+//		
+//		ArrayList<String> myDisabilities = new ArrayList<String>();
+//		myDisabilities.add("PTSD");
+//		myVeteran.setDisabilities(myDisabilities);
+//		
+//		ArrayList<String> myNeeds = new ArrayList<String>();
+//		myNeeds.add("Food");
+//		myNeeds.add("Clothing");
+//		myNeeds.add("Housing");
+//		myVeteran.setNeeds(myNeeds);
+//		
+//		ArrayList<String> myHistory = new ArrayList<String>();
+//		myHistory.add("null");
+//		myVeteran.setHistory(myHistory);
+//		
+//		
+//		System.out.println("myVeteran.name: " + myVeteran.getName());
+//		
+//		CaseWorker myCaseWorker = new CaseWorker();
+//		
+//		myCaseWorker.setName("Sicko Mo Bamba");
+//		myCaseWorker.setOrganization("Vet Foundation");
+//		ArrayList<Veteran> myVeteranList = new ArrayList<Veteran>();
+//		myVeteranList.add(myVeteran);
+//		myCaseWorker.setVeteranList(myVeteranList);
+//		
+//		System.out.println("myCaseWorker.organization: " + myCaseWorker.getOrganization());
 	}
 
 }
--- anthony/src/main/java/com/example/codeLinc6/controller/VeteranController.java
+++ anthony/src/main/java/com/example/codeLinc6/controller/VeteranController.java
@@ -1,19 +1,36 @@
 package com.example.codeLinc6.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.codeLinc6.mapperManager.VeteranMapperManager;
 import com.example.codeLinc6.model.Veteran;
 
 @RestController
+@Component
+@RequestMapping("/codeLinc6")
 public class VeteranController {
 	
 	//Configure your object to use in order to use
 	@Autowired
 	VeteranMapperManager veteranMapperManager;
+	
+	@GetMapping("/test")
+	public String adVeteran() {
+		
+		//You can use the veteran param with the below, to call any methods in it
+		
+		veteranMapperManager.getAll();//call whatever methods you want
+		
+		
+		return "works";
+	}
+	
 	
 	@PostMapping("/veteran")
 	public String addVeteran(@RequestBody Veteran veteran) {
@@ -25,5 +42,7 @@ public class VeteranController {
 		
 		return "works";
 	}
+	
+	
 
 }
