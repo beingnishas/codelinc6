3ad5aa2bb70f925aed82be4a679e25e5d374a5e2
Malcolm R. Jones <maljonesdev@gmail.com>
2019-09-29 08:10:24 -0400

    Search functionality

--- nisha/js/services.js
+++ nisha/js/services.js
@@ -1,32 +1,32 @@
 const HTTP = new XMLHttpRequest();
-<<<<<<< HEAD
 const URL = "distance/test";
 
 var searchButton = document.getElementById("search-btn");
 
 searchButton.onclick = function() 
 {
-=======
 const URL = "http://localhost:8080/services/search?filter=";
 
 var searchButton = document.getElementById("search-btn");
-
+var results = document.getElementById("results");
 
 searchButton.onclick = function() {
->>>>>>> ad188810fa432743ca0093a80c5952a2a6f681f5
-    
-    var query = document.getElementById("search-input").innerHTML;
 
-    HTTP.open("GET", URL + query);
-    HTTP.send();
+    results.style.visibility = "visible";
+
     
-    HTTP.onreadystatechange = function() 
-    {
-        if(this.readyState == 4 && this.status == 200) 
-        {
-            console.log(HTTP.responseText);
-        }
-    }
+    // var query = document.getElementById("search-input").innerHTML;
+
+    // HTTP.open("GET", URL + query);
+    // HTTP.send();
+    
+    // HTTP.onreadystatechange = function() 
+    // {
+    //     if(this.readyState == 4 && this.status == 200) 
+    //     {
+    //         console.log(HTTP.responseText);
+    //     }
+    // }
 }
 
 
@@ -40,7 +40,7 @@ veteranPhoneButton.onclick = function()
     {
         if (this.readyState == 4 && this.status == 200)
         {
-            console.log(HTTP.)
+        
         }
     }
 }
